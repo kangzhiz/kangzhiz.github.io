79f6290728b5280841680633e5ea7d20c5fbbb32
kangzhiz <6843803-kangzhiz@users.noreply.gitlab.com>
2020-09-08 17:55:26 -0500

    Removed checkboxes

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,7 +1,6 @@
 <template>
-	<div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+	<div class="todo-item">
 		<p>
-			<input type="checkbox" v-on:change="markComplete" />
 			{{todo.title}}
 			<button @click="$emit('del-todo', todo.id)" class="del">x</button>
 		</p>
@@ -12,11 +11,6 @@
 export default {
 	name: "TodoItem",
 	props: ["todo"],
-	methods: {
-		markComplete() {
-			this.todo.completed = !this.todo.completed;
-		},
-	},
 };
 </script>
 
@@ -27,27 +21,13 @@ export default {
 	border-bottom: 1px #cccccc dotted;
 	font-size: 17px;
 }
-.is-complete {
-	text-decoration: line-through;
-}
 .del {
-	background: #555555;
+	background: #ff2727;
 	color: #ffffff;
 	border: none;
 	padding: 5px 9px;
 	border-radius: 50%;
 	cursor: pointer;
 	float: right;
-}
-input[type="checkbox"] {
-	-webkit-appearance: none;
-	width: 15px;
-	height: 15px;
-	background: #ffffff;
-	border-radius: 100%;
-	border: 2px solid #555555;
-}
-input[type="checkbox"]:checked {
-	background: #6b6b6b;
 }
 </style>
