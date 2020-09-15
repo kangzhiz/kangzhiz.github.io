2495f2f9b7b9b0cfa01c450ed487dd1739c2744d
kangzhiz <6843803-kangzhiz@users.noreply.gitlab.com>
2020-09-15 12:49:41 -0500

    Fixed indents on js files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://kangzhiz.gitlab.io/'
-    : '/'
+	publicPath: process.env.NODE_ENV === 'production'
+		? 'https://kangzhiz.gitlab.io/'
+		: '/'
 }
