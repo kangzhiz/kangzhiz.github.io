a938efcf7e45e5c95a89f3e45d773854747ea8ba
kangzhiz <kangzhiz@pm.me>
2020-12-31 21:55:01 -0600

    Slight modifications

--- scripts/site.js
+++ scripts/site.js
@@ -1,4 +1,23 @@
+/*
+ * Returns true if display is set to none.
+ */
+function isHidden(el) {
+    return window.getComputedStyle(el).display === 'none';
+}
+
+/*
+ * Toggle visibility of command box.
+ *
+ * TODO: smooth animations
+ */
 function toggleCmd() {
-    $('#logo input').toggle('medium');
-    $('#logo input').focus();
+    let cmdbox = document.getElementById('cmdbox');
+    let txtfield = cmdbox.getElementsByTagName('input')[0];
+
+    if (isHidden(cmdbox)) {
+        cmdbox.style.display = 'block';
+        txtfield.focus();
+    } else {
+        cmdbox.style.display = 'none';
+    }
 }
