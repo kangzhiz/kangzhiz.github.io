f7caec2057622cf6b456f802f8a1a08860e8c97f
John Zhao <kangzhiz@pm.me>
2022-03-18 05:01:11 -0400

    Added script to concatenate text

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+/*
+ * Change name text to just initials for smaller screen sizes
+ */
+
+let spans = [];
+let fullNames = [];
+
+function concatText() {
+    let size = window.innerWidth;
+
+    for (let i = 0; i < spans.length; i++) {
+        let name = spans[i].textContent;
+        console.log(name);
+        spans[i].textContent = size > 876 ? fullNames[i] : name[0];
+    }
+};
+
+window.onload = function() {
+    spans = document.getElementsByClassName('firstlast');
+
+    // store full names in case the window size is increased
+    for (let i = 0; i < spans.length; i++) {
+        fullNames.push(spans[i].textContent);
+    }
+
+    concatText();
+}
+
+window.onresize = concatText;
+
