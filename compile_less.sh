88e6a1027ece62b629f68511a5ef872b909d153c
John Zhao <kangzhiz@pm.me>
2022-03-19 04:33:39 -0400

    Rebuild website in Svelte.js

--- compile_less.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# compile LESS to CSS - dump into dist/styles.css
-./node_modules/.bin/lessc css/styles.less dist/styles.css
---
+++ src/components/LinkContainer.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Link from './Link.svelte'
+</script>
+
+<div class='links-container'>
+    <h2 class='header'>LINKS:</h2>
+
+    <div class='links-group'>
+        <Link link='https://github.com/kangzhiz' text='github'/>
+        <Link link='https://www.linkedin.com/in/kangzhiz' text='linkedin'/>
+    </div>
+</div>
+
+<style>
+    .links-container {
+        border: 3px solid var(--gruv_dark0);
+        border-radius: 25px;
+        margin: auto;
+        width: 80%;
+    }
+
+    .header {
+        font-family: 'Archivo Black', Arial, Helvetica, sans-serif;
+    }
+
+    .links-group {
+        display: flex;
+        font-family: 'Cascadia Mono', monospaced;
+        justify-content: center;
+        line-height: 3rem;
+        padding-bottom: 2rem;
+    }
+
+    @media only screen and (max-width: 876px) {
+        .links-group {
+            flex-direction: column;
+        }
+    }
+</style>
